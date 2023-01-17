dc924b7822d30b47a66a89f8771d8a766c8d93c0
babaktaremi <babaktaremi@yahoo.com>
2023-01-18 01:32:59 +0330

    Add External Consent Application

--- Infrastructure/PersonaServer.Infrastructure.OpenIdDict/Workers/AddTestDataWorker.cs
+++ Infrastructure/PersonaServer.Infrastructure.OpenIdDict/Workers/AddTestDataWorker.cs
@@ -36,9 +36,54 @@ internal class AddTestDataWorker : BackgroundService
                     OpenIddictConstants.Permissions.Endpoints.Logout,
                     OpenIddictConstants.Permissions.Endpoints.Token,
 
+
+
+
                     OpenIddictConstants.GrantTypes.AuthorizationCode,
                     OpenIddictConstants.GrantTypes.RefreshToken,
                     OpenIddictConstants.GrantTypes.Implicit,
+
+                    OpenIddictConstants.ResponseTypes.IdToken,
+                    OpenIddictConstants.ResponseTypes.Code,
+
+                    OpenIddictConstants.Scopes.OpenId,
+                    OpenIddictConstants.Scopes.Email,
+                    OpenIddictConstants.Scopes.Profile,
+                    OpenIddictConstants.Scopes.Roles,
+                },
+                Requirements =
+                {
+                    OpenIddictConstants.Requirements.Features.ProofKeyForCodeExchange,
+                },
+                PostLogoutRedirectUris = { new Uri("https://localhost:7098/signout-callback-oidc") }
+            }, stoppingToken);
+        }
+
+        if (await manager.FindByClientIdAsync("main-Api-2", stoppingToken) is null)
+        {
+            await manager.CreateAsync(new OpenIddictApplicationDescriptor
+            {
+                ClientId = "main-Api-2",
+                ConsentType = OpenIddictConstants.ConsentTypes.External,
+                ClientSecret = "main-Api-Secret-2",
+                DisplayName = "Interaction With Main API 2",
+                Type = OpenIddictConstants.ClientTypes.Confidential,
+                RedirectUris = { new Uri("https://oauth.pstmn.io/v1/callback"), new Uri("https://localhost:5552"), new Uri("https://oidcdebugger.com/debug"), new Uri("https://localhost:7098/signin-oidc") },
+                Permissions =
+                {
+                    OpenIddictConstants.Permissions.GrantTypes.AuthorizationCode,
+                    OpenIddictConstants.Permissions.ResponseTypes.IdToken,
+                    OpenIddictConstants.Permissions.ResponseTypes.Code,
+                    OpenIddictConstants.Permissions.Scopes.Email,
+                    OpenIddictConstants.Permissions.Scopes.Profile,
+                    OpenIddictConstants.Permissions.Scopes.Roles,
+
+                    OpenIddictConstants.Permissions.Endpoints.Authorization,
+                    OpenIddictConstants.Permissions.Endpoints.Logout,
+                    OpenIddictConstants.Permissions.Endpoints.Token,
+
+                    OpenIddictConstants.GrantTypes.AuthorizationCode,
+                    OpenIddictConstants.GrantTypes.RefreshToken,
 
                     OpenIddictConstants.ResponseTypes.IdToken,
                     OpenIddictConstants.ResponseTypes.Code,
